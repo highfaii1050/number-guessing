2dc625409cf3f1c38af14c3a7e9611f6a519708f
Leong Chee Fai <leongcheefai0521@gmail.com>
2023-08-14 22:59:14 +0800

    Only show game over when guess chances is 0

--- number_guessing.py
+++ number_guessing.py
@@ -1,26 +1,28 @@
 import random
 
 n = random.randint(1, 99)
-count = 1
-guess_chances = 5
+min, max, count, guess_chances = 1, 99, 1, 5
 player_name = input("Hello, What's your name? ")
 
 print("Hello," + player_name)
 
 while 1 <= guess_chances:
-    num = int(input("Enter an integer from 1 to 99: "))
+    num = int(input(f"Enter an integer from {min} to {max}: "))
     if num > n:
-        print("guess is too high. Try guess something lower than", num)
+        print("Your guess is too high. Try guess something lower than", num)
+        max = num
     elif num < n:
-        print("guess is too low. Try guess something higher than", num)
+        print("Your guess is too low. Try guess something higher than", num)
+        min = num
     else:
-        print("you guessed it!")
-        print("you took ", str(count), " tries to guess the number")
+        print("You guessed it!")
+        print("You took", str(count), "tries to guess the number")
         break
     guess_chances -= 1
-    print("you have ", str(guess_chances), " chances left")
+    print("you have", str(guess_chances), "chances left")
     count += 1
 
-print("Game Over")
-print("The number is ", str(n))
-print("Thanks for playing")
+if guess_chances == 0:
+    print("Game Over")
+    print("The number is", str(n))
+    print("Thanks for playing")
